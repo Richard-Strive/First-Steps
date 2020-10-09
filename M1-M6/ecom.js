8c3e9827b920f91517de0370445da0fece193345
Richard Johnson Addai <72205808+Richard-Strive@users.noreply.github.com>
2020-10-09 22:11:48 +0200

    Add files via upload

---
+++ M1-M6/ecom.js
@@ -0,0 +1,26 @@
+
+//let ref= document.querySelector("body");
+
+
+
+//ref.window.alert("Loading Compleated")
+
+let myFunction= function(){
+
+window.alert("Welcome to my first project")
+
+};
+ 
+  const changeTitle = function (newTitle) {
+
+        document.querySelector("h1")= newTitle
+
+let ref= document.querySelector("h1")= newTitle
+
+ref.innerText=" "
+
+  }
+
+
+
+  changeTitle("Here it is")
